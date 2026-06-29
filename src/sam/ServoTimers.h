6855a13e289cd3d38e0f769d25891a49a39dc7c4
Shin Taeha <mirinae3145@g.skku.edu>
2026-06-29 20:45:17 +0900

    Refactor timer definitions to use conditional compilation for better flexibility

--- src/sam/ServoTimers.h
+++ src/sam/ServoTimers.h
@@ -30,11 +30,21 @@
  */
 
 // For SAM3X:
-#define _useTimer1
-#define _useTimer2
-#define _useTimer3
-#define _useTimer4
-#define _useTimer5
+#ifndef _useTimer1
+#define _useTimer1 1
+#endif
+#ifndef _useTimer2
+#define _useTimer2 1
+#endif
+#ifndef _useTimer3
+#define _useTimer3 1
+#endif
+#ifndef _useTimer4
+#define _useTimer4 1
+#endif
+#ifndef _useTimer5
+#define _useTimer5 1
+#endif
 
 /*
   TC0, chan 0 => TC0_Handler
@@ -48,35 +58,35 @@
   TC2, chan 2 => TC8_Handler
  */
 
-#if defined (_useTimer1)
+#if defined(_useTimer1) && (_useTimer1)
 #define TC_FOR_TIMER1       TC1
 #define CHANNEL_FOR_TIMER1  0
 #define ID_TC_FOR_TIMER1    ID_TC3
 #define IRQn_FOR_TIMER1     TC3_IRQn
 #define HANDLER_FOR_TIMER1  TC3_Handler
 #endif
-#if defined (_useTimer2)
+#if defined(_useTimer2) && (_useTimer2)
 #define TC_FOR_TIMER2       TC1
 #define CHANNEL_FOR_TIMER2  1
 #define ID_TC_FOR_TIMER2    ID_TC4
 #define IRQn_FOR_TIMER2     TC4_IRQn
 #define HANDLER_FOR_TIMER2  TC4_Handler
 #endif
-#if defined (_useTimer3)
+#if defined(_useTimer3) && (_useTimer3)
 #define TC_FOR_TIMER3       TC1
 #define CHANNEL_FOR_TIMER3  2
 #define ID_TC_FOR_TIMER3    ID_TC5
 #define IRQn_FOR_TIMER3     TC5_IRQn
 #define HANDLER_FOR_TIMER3  TC5_Handler
 #endif
-#if defined (_useTimer4)
+#if defined(_useTimer4) && (_useTimer4)
 #define TC_FOR_TIMER4       TC0
 #define CHANNEL_FOR_TIMER4  2
 #define ID_TC_FOR_TIMER4    ID_TC2
 #define IRQn_FOR_TIMER4     TC2_IRQn
 #define HANDLER_FOR_TIMER4  TC2_Handler
 #endif
-#if defined (_useTimer5)
+#if defined(_useTimer5) && (_useTimer5)
 #define TC_FOR_TIMER5       TC0
 #define CHANNEL_FOR_TIMER5  0
 #define ID_TC_FOR_TIMER5    ID_TC0
@@ -84,4 +94,20 @@
 #define HANDLER_FOR_TIMER5  TC0_Handler
 #endif
 
-typedef enum { _timer1, _timer2, _timer3, _timer4, _timer5, _Nbr_16timers } timer16_Sequence_t ;
+typedef enum {
+#if defined(_useTimer1) && (_useTimer1)
+_timer1,
+#endif
+#if defined(_useTimer2) && (_useTimer2)
+_timer2,
+#endif
+#if defined(_useTimer3) && (_useTimer3)
+_timer3,
+#endif
+#if defined(_useTimer4) && (_useTimer4)
+_timer4,
+#endif
+#if defined(_useTimer5) && (_useTimer5)
+_timer5,
+#endif
+_Nbr_16timers } timer16_Sequence_t ;
